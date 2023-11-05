a9015e877ca67ecf6728b30cda0fbfe3077f5b7e
dorantdushi <dorantdushi@gmail.com>
2023-11-05 10:31:53 +0100

    products on development

--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -12,16 +12,6 @@ export class ProductsComponent {
       description: 'This is a creative product description for Product 1.',
       image: 'assets/product1.jpg'
     },
-    {
-      name: 'Product 2',
-      description: 'Discover the amazing features of Product 2.',
-      image: 'assets/product2.jpg'
-    },
-    {
-      name: 'Product 3',
-      description: 'Get ready to be impressed by Product 3.',
-      image: 'assets/product3.jpg'
-    }
     // Add more products here
   ];
 }
